5265763f0e111c2b6bfac4c2a5d0e9793dea0fa8
vladimir-kirillovskiy <vladimir.kirillovskiy@gmail.com>
2024-10-14 01:28:53 +0600

    completed project

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+    bg: {
+        type: String,
+        default: 'bg-grey-100'
+    }
+});
+</script>
+
+<template>
+    <div :class="`${bg} p-6 rounded-lg shadow-md`">
+        <slot></slot>
+    </div>
+
+</template>
